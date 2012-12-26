4d519b89398ec98aabccf69ea078b2fba5dd88a7
Oleh Halenok <o.halenok@gmail.com>
2012-12-26 12:34:33 +0200

    third

--- ntp/manifests/install.pp
+++
@@ -1,9 +0,0 @@
-class ntp::install {
-  # If ntp_manage_install = false then do nothing.
-  # For better resolving dependencies cycles.
-  if str2bool("$ntp::ntp_manage_install") {
-	  package { $ntp::ntp_package:
-	    ensure => $ntp::ntp_package_ensure,
-	  }
-  }
-}
--- test/manifests/init.pp
+++
@@ -1,7 +0,0 @@
-##
-class test () {
-  if $test_variable == false {
-    $test_variable = 'Hello World'
-  }
-  notify {$test_variable:}
-}
