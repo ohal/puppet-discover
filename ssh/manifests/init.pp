4e22b9299b9aa2550342338bfefd402e4a78e85f
Oleh Halenok <o.halenok@gmail.com>
2013-01-14 18:48:17 +0200

    ohal ssh 0.1

--- ssh/manifests/init.pp
+++ ssh/manifests/init.pp
@@ -7,17 +7,47 @@ class ssh (
   $ssh_ensure = 'present',
   # port is open for external connection to openssh server
   $ssh_port = '22',
+  # ssh protocol
+  $ssh_protocol = '2',
   # openssh server is listen from ip addresses
   $ssh_listen_address = [],
+  # privilege separation
+  $ssh_privilege_separation = 'yes',
+  # loglevel - the possible values are: 
+  # QUIET, FATAL, ERROR, INFO, VERBOSE, DEBUG, DEBUG1, DEBUG2, and DEBUG3
+  # default is INFO.
+  $ssh_loglevel = 'INFO',
+  # server disconnects after this time in seconds
+  $ssh_logingracetime = '120',
+  # should check file modes and ownership before accepting login
+  $ssh_strictmodes = 'yes',
   # root connections restriction
   $ssh_root_login = 'yes',
   # clear password restriction
   $ssh_password_authentication = 'no',
   # users what allowed to connect to openssh server
   $ssh_users = [],
-  # to enable empty passwords, change to yes (NOT RECOMMENDED)
+  # specifies whether pure RSA authentication is allowed
+  $ssh_rsa_authentication = 'yes',
+  # specifies whether public key authentication is allowed
+  $ssh_pubkey_authentication = 'yes',
+  # specifies that .rhosts and .shosts files will not be used
+  $ssh_ignore_rhosts = 'yes',
+  # specifies whether rhosts or /etc/hosts.equiv authentication together
+  # with successful RSA host authentication is allowed
+  $ssh_rhostsrsa_authentication = 'no',
+  # specifies whether rhosts or /etc/hosts.equiv authentication
+  # together with successful public key client host authentication is
+  # allowed (host-based authentication)
+  # protocol version 2 only
+  $ssh_hostbased_authentication = 'no',
+  # specifies whether should ignore the user's ~/.ssh/known_hosts
+  $ssh_ignore_user_known_hosts = 'no',
+  # specifies whether challenge-response authentication is allowed
+  $ssh_challenge_response_authentication = 'yes',
+  # to enable empty passwords change to yes (NOT RECOMMENDED)
   $ssh_empty_password = 'no',
-  # addition
+  # defaults
   $ssh_manage_install = true,
   $ssh_manage_config = true,
   $ssh_manage_service = true,
@@ -44,14 +74,14 @@ class ssh (
   # set packages list depends on OS
   if ! $ssh_packages {
       $ssh_packages_real = $::osfamily ? {
-        'Debian' => hiera('ssh_packages',
-          ['ssh','openssh-client','openssh-server']
-        ),
-        'RedHat' => hiera('ssh_packages',
-          ['openssh','openssh-clients','openssh-server']
-        ),
-        default  => hiera('ssh_packages',undef),
-        }
+          'Debian' => hiera('ssh_packages',
+                        ['ssh','openssh-client','openssh-server']
+                      ),
+          'RedHat' => hiera('ssh_packages',
+                        ['openssh','openssh-clients','openssh-server']
+                      ),
+           default => hiera('ssh_packages',undef),
+      }
   } else {
       $ssh_packages_real = $ssh_packages
   }
