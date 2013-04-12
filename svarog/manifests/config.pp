ec0d9fe96771ce74f43d1e3f0ad486dd16ba6367
Oleh Halenok <o.halenok@gmail.com>
2013-04-12 17:37:04 +0300

    current

--- svarog/manifests/config.pp
+++ svarog/manifests/config.pp
@@ -2,12 +2,44 @@ class svarog::config {
   # If svarog_manage_config = false then do nothing.
   # For better resolving dependencies cycles.
   if str2bool("$svarog::svarog_manage_config") {
-    file { $svarog::svarog_config_file:
+    file { $svarog::svarog_config_dir:
+      ensure  => $svarog::svarog_ensure_dir,
+      owner   => $svarog::svarog_config_owner,
+      group   => $svarog::svarog_config_group,
+      mode    => $svarog::svarog_config_settings_dir_mode,
+    }
+    file { $svarog::svarog_config_settings_dir:
+      ensure  => $svarog::svarog_ensure_dir,
+      owner   => $svarog::svarog_config_owner,
+      group   => $svarog::svarog_config_group,
+      mode    => $svarog::svarog_config_settings_dir_mode,
+    }
+    file { $svarog::svarog_config_dummy_dir:
+      ensure  => $svarog::svarog_ensure_dir,
+      owner   => $svarog::svarog_config_owner,
+      group   => $svarog::svarog_config_group,
+      mode    => $svarog::svarog_config_dummy_dir_mode,
+    }
+    file { $svarog::svarog_config_repo:
       ensure  => $svarog::svarog_ensure,
-      owner   => $svarog::svarog_config_file_owner,
-      group   => $svarog::svarog_config_file_group,
-      mode    => $svarog::svarog_config_file_mode,
+      owner   => $svarog::svarog_config_owner,
+      group   => $svarog::svarog_config_group,
+      mode    => $svarog::svarog_config_repo_mode,
       content => template('svarog/local.repo.erb'),
+    }
+    file { $svarog::svarog_config_settings:
+      ensure  => $svarog::svarog_ensure,
+      owner   => $svarog::svarog_config_owner,
+      group   => $svarog::svarog_config_group,
+      mode    => $svarog::svarog_config_settings_mode,
+      content => template('svarog/settings.sh.erb'),
+    }
+    file { $svarog::svarog_config_dummy:
+      ensure  => $svarog::svarog_ensure,
+      owner   => $svarog::svarog_config_owner,
+      group   => $svarog::svarog_config_group,
+      mode    => $svarog::svarog_config_dummy_mode,
+      content => template('svarog/ssof-dummy-settings.sh.erb'),
     }
   }
 }
--- svarog/manifests/init.pp
+++ svarog/manifests/init.pp
@@ -5,16 +5,27 @@
 class svarog (
   # defaults
   $svarog_ensure = 'present',
+  $svarog_ensure_dir = 'directory',
   $svarog_autoupgrade = true,
   $svarog_packages = undef,
   $svarog_manage_install = true,
   $svarog_manage_config = true,
-  $svarog_config_file = '/etc/yum.repos.d/local.repo',
-  $svarog_config_file_owner = 'root',
-  $svarog_config_file_group = 'root',
-  $svarog_config_file_mode = '0644',
+  $svarog_config_owner = 'root',
+  $svarog_config_group = 'root',
   # local repository url
   $svarog_repo_url = 'http://192.168.153.38/repo/',
+  $svarog_config_repo = '/etc/yum.repos.d/local.repo',
+  $svarog_config_repo_mode = '0644',
+  # svarog initial general conf
+  $svarog_config_dir = '/etc/softserve',
+  $svarog_config_settings_dir = '/etc/softserve/general',
+  $svarog_config_settings_dir_mode = '755',
+  $svarog_config_settings = '/etc/softserve/general/settings.sh',
+  $svarog_config_settings_mode = '0755',
+  $svarog_config_dummy_dir = '/etc/softserve/general/settings.d',
+  $svarog_config_dummy_dir_mode = '755',
+  $svarog_config_dummy = '/etc/softserve/general/settings.d/ssof-dummy-settings.sh',
+  $svarog_config_dummy_mode = '0755',
     ) {
   # set svarog packages list
   if ! $svarog_packages {
